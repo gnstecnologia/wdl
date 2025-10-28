8a7772bcd16574a7cdc95eafaf6bf40431328390
Genesis Tecnologia <gnstecnologia@example.com>
2025-10-28 14:43:20 -0300

    feat: Troca imagens para que cada pÃ¡gina tenha uma imagem Ãºnica

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -6,7 +6,7 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Mail, Phone, Instagram, Facebook, Linkedin, Youtube } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
-import heroImage from "@/assets/hero-curadoria.jpg";
+import heroImage from "@/assets/hero-evento.jpg";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
